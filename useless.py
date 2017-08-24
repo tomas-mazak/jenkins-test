5e926fbf696ac6efc300f514c3df2903d63e3c31
Tomas Mazak <tomas@valec.net>
2017-08-24 23:42:53 +0100

    Add something to test

---
+++ useless.py
@@ -0,0 +1,14 @@
+import math
+from typing import Tuple
+
+
+def quadratic(a: float, b: float, c: float) -> Tuple[float, float]:
+    """
+    Compute roots of a quadratic function:
+
+        a*x**2 + b*x + c == 0
+    """
+    discriminant = math.sqrt(b**2.0 - 4.0*a*c)
+    x1 = (-b + discriminant) / (2.0 * a)
+    x2 = (-b - discriminant) / (2.0 * a)
+    return x1, x2
