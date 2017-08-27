64ac3b2f92acb14fba0cbe09c225055683eec027
Tomas Mazak <tomas@valec.net>
2017-08-27 19:14:32 +0100

    Aaand uncomment agaiiin!

--- useless.py
+++ useless.py
@@ -20,8 +20,8 @@ def add(a: float, b: float) -> float:
     """
     return a + b
 
-#def mul(a: float, b: float) -> float:
-#    """
-#    Multiply two numbers
-#    """
-#    return a * b
+def mul(a: float, b: float) -> float:
+    """
+    Multiply two numbers
+    """
+    return a * b
