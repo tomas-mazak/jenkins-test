92513fdd26c3b487fbfba5d4584140121f9e8e5f
Tomas Mazak <tomas@valec.net>
2017-08-27 20:00:10 +0100

    Test N+2

--- useless.py
+++ useless.py
@@ -19,3 +19,9 @@ def add(a: float, b: float) -> float:
     Add two numbers
     """
     return a + b
+
+def mul(a: float, b: float) -> float:
+    """
+    Multiply two numbers
+    """
+    return a * b
